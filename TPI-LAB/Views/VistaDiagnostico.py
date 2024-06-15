6226723d687683198d6a772613295cedc4ce46f1
Jeronimo <101224621+JeritoB@users.noreply.github.com>
2024-06-15 03:53:35 -0300

    Delete TPI-LAB directory

--- TPI-LAB/Views/VistaDiagnostico.py
+++
@@ -1,7 +0,0 @@
-class VistaDiagnostico:
-    def pregunta(self):
-        pregunta = int(input("Ingrese el codigo del animal para el diagnostico: "))
-        return pregunta
-    def diagnosticoNuevo(self):
-        diagnostico = input("Cree el nuevo diagnostico: ")
-        return diagnostico
